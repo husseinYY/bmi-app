2112816ae447af637111d0a33f6b88322ef1b043
husseinYY <usetit.inc@gmail.com>
2021-02-16 11:09:38 +0300

    final considirations and changing splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:responsive_framework/responsive_framework.dart';
+
 import 'InputPage.dart';
 
 void main() => runApp(BMICalculator());
@@ -10,6 +13,16 @@ class BMICalculator extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
             primaryColor: Color(0xFF0A0E21),
             scaffoldBackgroundColor: Color(0xFF0A0E21)),
+        builder: (context, widget) => ResponsiveWrapper.builder(widget,
+            maxWidth: 1200,
+            minWidth: 480,
+            defaultScale: true,
+            breakpoints: [
+              ResponsiveBreakpoint.resize(480, name: MOBILE),
+              ResponsiveBreakpoint.autoScale(800, name: TABLET),
+              ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+            ],
+            background: Container(color: Color(0xFF0A0E21))),
         home: InputPage());
   }
 }
